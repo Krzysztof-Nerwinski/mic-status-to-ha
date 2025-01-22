50c57c01cb39476b25c20e05ea940c559d1fce37
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2025-01-22 13:51:11 +0100

    fix: use pulse audio control utils to monitor input device
    /proc/asound/card0/pcm0p/sub0/status is changing status for output devices which makes this approach unfeasible

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Microphone status to Home Assistant synchronizer
 
-This is a simple Python script that synchronizes the status of a microphone inside Linux OS 
-with Home Assistant. 
+This is a simple Python script that synchronizes the status of a microphone inside Linux OS
+with Home Assistant.
 It checks microphone status inside: /proc/asound/card0/pcm0p/sub0/status file and then sends it to Home Assistant
 using a REST API call.
 
@@ -9,12 +9,24 @@ using a REST API call.
 
 ## Home Assistant configuration
 
-1. Create long-lived access token for your account: https://developers.home-assistant.io/docs/auth_api/#making-authenticated-requests
-2. Create helper boolean input that will represent mic status: https://www.home-assistant.io/integrations/input_boolean/
+1. Create long-lived access token for your
+   account: https://developers.home-assistant.io/docs/auth_api/#making-authenticated-requests
+2. Create helper boolean input that will represent microphone status: https://www.home-assistant.io/integrations/input_boolean/
+
+## Getting microphone device name
+
+1. Install `pactl` tool by running:
+   ```bash
+   sudo apt update
+   sudo apt install pulseaudio-utils
+   ```
+   
+2. Find your monitored microphone device name by using `pactl list sources` command. 
+   Example device name: `alsa_input.usb-046d_Brio_500_2238LZ515048-02.iec958-stereo`
 
 ## Systemd Service Configuration
 
-To use the provided `mic-status-to-ha.service.example` file, follow these steps:
+To start the script on system startup, follow these steps:
 
 1. Copy example file by removing `.example` from its name:
    ```bash
@@ -23,31 +35,44 @@ To use the provided `mic-status-to-ha.service.example` file, follow these steps:
 
 2. Edit the `mic-status-to-ha.service` file and replace the following:
 
-   * Change username in `User` directive
    * Change script path in `ExecStart` directive
    * Change `HA_URL` to your ha instance url
    * Change `HA_BEARER_TOKEN` to your ha long-lived access token
    * Change `HA_ENTITY_ID` to your ha entity id
+   * Change `MONITORED_DEVICE` to your monitored device name
 
-3. Move the renamed file to the Systemd directory as root:
+3. Move the renamed file to the Systemd user directory as root:
    ```bash
-   sudo mv mic-status-to-ha.service /etc/systemd/system/
+   sudo mv mic-status-to-ha.service /etc/systemd/user/
+   ```
+   
+4. Reload systemd user service:
+   ```bash
+   systemctl --user daemon-reload
    ```
 
-4. Enable the service to start on boot:
+5. Enable the service to start on boot:
    ```bash
-   sudo systemctl enable mic-status-to-ha.service
+   systemctl --user enable mic-status-to-ha.service
    ```
 
-5. Start the service:
+6. Start the service:
    ```bash
-   sudo systemctl start mic-status-to-ha.service
+   systemctl start mic-status-to-ha.service
    ```
 
-You can verify the status of the service using:
+7. Verify the status of the service using:
+   ```bash
+   systemctl --user status mic-status-to-ha.service
+   ```
 
+# Running the script manually
+
+Export all required variables to environment and run the script:
 ```bash
-sudo systemctl status mic-status-to-ha.service
+export HA_URL=
+export HA_BEARER_TOKEN=
+export HA_ENTITY_ID=
+export MONITORED_DEVICE=
+python3 mic_status_to_ha.py
 ```
-
-
--- mic-status-to-ha.py
+++ mic-status-to-ha.py
@@ -1,26 +1,45 @@
+#!/usr/bin/env python3
+
 import json
 import logging
 import os
+import re
 from argparse import ArgumentParser
+from dataclasses import dataclass, field
 from http.client import HTTPException
+from subprocess import check_output
 from time import sleep
 from typing import Optional
 from urllib.request import Request, urlopen
 
-# Constants
-HA_URL = os.environ["HA_URL"]
-BEARER_TOKEN = os.environ["HA_BEARER_TOKEN"]
-HEADERS = {
-    "Authorization": f"Bearer {BEARER_TOKEN}",
-    "Content-Type": "application/json",
-}
-ENTITY_ID = os.environ["HA_ENTITY_ID"]
-MIC_STATUS_FILE_PATH = "/proc/asound/card0/pcm0p/sub0/status"
+
+@dataclass
+class Config:
+    ha_url: str
+    ha_bearer_token: str
+    ha_entity_id: str
+    monitored_device_name: str
+    log_level: str = "INFO"
+    ha_headers: dict = field(init=False)
+
+    def __post_init__(self):
+        if not self.ha_url:
+            raise ValueError("Home Assistant URL is required")
+        if not self.ha_bearer_token:
+            raise ValueError("Bearer token is required")
+        if not self.ha_entity_id:
+            raise ValueError("Entity ID is required")
+        if not self.monitored_device_name:
+            raise ValueError("Monitored device name is required")
+        self.ha_headers = {
+            "Authorization": f"Bearer {self.ha_bearer_token}",
+            "Content-Type": "application/json",
+        }
 
 
-def get_entity_data(entity_id: str) -> Optional[dict]:
+def get_entity_data(config: Config) -> Optional[dict]:
     """Fetches the current state of the provided entity."""
-    request = Request(f"{HA_URL}/api/states/{entity_id}", headers=HEADERS, method="GET")
+    request = Request(f"{config.ha_url}/api/states/{config.ha_entity_id}", headers=config.ha_headers, method="GET")
     try:
         with urlopen(request) as response:
             response_data_str = response.read().decode("utf-8")
@@ -30,11 +49,11 @@ def get_entity_data(entity_id: str) -> Optional[dict]:
     return json.loads(response_data_str)
 
 
-def change_entity_data(entity_id: str, updated_state: dict) -> Optional[dict]:
+def change_entity_data(config: Config, updated_state: dict) -> Optional[dict]:
     """Updates the state of the given entity."""
     request = Request(
-        f"{HA_URL}/api/states/{entity_id}",
-        headers=HEADERS,
+        f"{config.ha_url}/api/states/{config.ha_entity_id}",
+        headers=config.ha_headers,
         method="POST",
         data=json.dumps(updated_state).encode("utf-8"),
     )
@@ -47,19 +66,49 @@ def change_entity_data(entity_id: str, updated_state: dict) -> Optional[dict]:
     return json.loads(response_data_str)
 
 
-def get_mic_status() -> bool:
+def get_names_with_states_from_pa_ctl_stdout(cmd_output: str):
+    devices = []
+    device_pattern = r"Source #\d+\n\tState: (\w+)\n\tName: (.+?)\n"
+
+    matches = re.finditer(device_pattern, cmd_output)
+    for match in matches:
+        state = match.group(1)
+        name = match.group(2)
+        devices.append({"name": name, "state": state})
+
+    return devices
+
+
+def get_mic_status(device_name: str) -> bool:
     """Returns True if the mic is active, and False otherwise."""
-    with open(MIC_STATUS_FILE_PATH, "r") as mic_status_file:
-        return "closed" not in mic_status_file.read()
+    try:
+        res = check_output(["/bin/pactl", "list", "sources"], user="krzysztof").decode("utf-8")
+    except Exception as e:
+        logging.error(f"Failed to get mic status: {e}")
+        return False
+    devices = get_names_with_states_from_pa_ctl_stdout(res)
+    logging.debug(f"Found devices: {devices}")
+
+    monitored_device = next((d for d in devices if d["name"] == device_name), None)
+    logging.debug(f"Monitored device: {monitored_device}")
+
+    if not monitored_device:
+        logging.error(f"Device {device_name} not found in devices: {devices}")
+        return False
+
+    logging.debug(f"Monitored device state: {monitored_device['state']}")
+    return monitored_device["state"] == "RUNNING"
 
 
-def main():
+def main(config: Config):
     """Monitors mic status and updates the Home Assistant entity."""
+    logging.basicConfig(level=config.log_level)
+
     entity_data = None
 
     while True:
         if entity_data is None:
-            entity_data = get_entity_data(ENTITY_ID)
+            entity_data = get_entity_data(config)
             if entity_data is None:
                 sleep(5)
                 continue
@@ -67,7 +116,7 @@ def main():
                 logging.info(f"Initial entity state: {entity_data}")
 
         ha_mic_state_is_on = entity_data.get("state") == "on"
-        mic_is_on = get_mic_status()
+        mic_is_on = get_mic_status(config.monitored_device_name)
 
         if mic_is_on != ha_mic_state_is_on:
             new_state = {
@@ -75,7 +124,7 @@ def main():
                 "entity_id": entity_data["attributes"],
                 "state": "on" if mic_is_on else "off"
             }
-            entity_data = change_entity_data(ENTITY_ID, new_state)
+            entity_data = change_entity_data(config, new_state)
             logging.info(f"Changed entity state to: {entity_data}")
         else:
             logging.debug("No change in mic status")
@@ -83,13 +132,19 @@ def main():
         sleep(3)
 
 
-def get_and_parse_args():
-    args = ArgumentParser()
-    args.add_argument("--log-level", default="INFO", help="Logging level", choices=["DEBUG", "INFO"])
-    return args.parse_args()
+def parse_config() -> Config:
+    parser = ArgumentParser()
+    parser.add_argument("--log-level", default="INFO", help="Logging level", choices=["DEBUG", "INFO"])
+    args = parser.parse_args()
+
+    return Config(
+        ha_url=os.environ.get("HA_URL"),
+        ha_bearer_token=os.environ.get("HA_BEARER_TOKEN"),
+        ha_entity_id=os.environ.get("HA_ENTITY_ID"),
+        monitored_device_name=os.environ.get("MONITORED_DEVICE"),
+        log_level=args.log_level)
 
 
 if __name__ == "__main__":
-    parsed_args = get_and_parse_args()
-    logging.basicConfig(level=parsed_args.log_level)
-    main()
+    parsed_config = parse_config()
+    main(parsed_config)
